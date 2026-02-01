34eb10d96bf6c7861d77498d9e89f5d372bf9b4c
Didier Daloze <dalozedidier@gmail.com>
2026-02-01 23:25:41 +0100

    Add files via upload

--- patch_nulltrace_mypy.py
+++ patch_nulltrace_mypy.py
@@ -6,16 +6,10 @@ from pathlib import Path
 
 def _remove_collection_from_typing_imports(text: str) -> str:
     # from typing import A, B, Collection, C  -> remove Collection
-    pat1 = (
-        r"(from\s+typing\s+import\s+[^\n]*?)"
-        r"\bCollection\b\s*,\s*"
-    )
+    pat1 = r"(from\s+typing\s+import\s+[^\n]*?)" r"\bCollection\b\s*,\s*"
     text = re.sub(pat1, r"\1", text)
 
-    pat2 = (
-        r"(from\s+typing\s+import\s+[^\n]*?)"
-        r",\s*\bCollection\b"
-    )
+    pat2 = r"(from\s+typing\s+import\s+[^\n]*?)" r",\s*\bCollection\b"
     text = re.sub(pat2, r"\1", text)
 
     pat3 = r"(from\s+typing\s+import\s+)\bCollection\b(\s*\n)"
